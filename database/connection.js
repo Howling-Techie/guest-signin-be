d324a38a4019bd9310a3b6060039f08b00bcd41e
Howling Techie <howling.techie@gmail.com>
2024-04-03 12:54:54 +0100

    Removed console logs

--- database/connection.js
+++ database/connection.js
@@ -3,8 +3,6 @@ const path = require("path");
 const db = new sqlite(path.resolve("guests.db"), {fileMustExist: true});
 
 function query(sql, params = []) {
-    console.log(sql);
-    console.log(params);
     return db.prepare(sql).all(params);
 }
 
